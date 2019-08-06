be556d8d3b53355dd5325967e99ecf6cbe68ef61
Fan Zeng <fanzengau@gmail.com>
2019-08-06 13:02:19 +1000

    bare bone

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');     
+const util = require('util');
+const express = require('express');
+const uuidv4 = require('uuid/v4');
+const readFile = util.promisify(fs.readFile);
+
+const port = process.env.PORT || 8081;
+
+var app = express();
+app.use(express.urlencoded({ extended: false }));
+console.log(__dirname + '/public_html')
+app.use(express.static(__dirname + '/public_html'));
+app.get('/index.js', function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello World\n');
+})
+app.post('/index.js', function (request, response) {
+  console.log(request.body);
+});
+const server = app.listen(port);
+
