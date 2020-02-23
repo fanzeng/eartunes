6734d799bcba89a8e6676de45aec0334a360b7fe
Fan Zeng <fanzengau@gmail.com>
2020-02-23 13:36:35 +1000

    fix audioCtx issue with ios safari

--- public/script/index.js
+++ public/script/index.js
@@ -1,25 +1,44 @@
 
 let stopped = true
-let microphone_off = true;
+let microphoneOff = true;
 let gumStream;
 let canvasTime = document.querySelector('.visualizer_time');
 let canvasFreq = document.querySelector('.visualizer_freq');
+let requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame || 
+                            window.webkitRequestAnimationFrame || 
+                            window.oRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
 
-function drawPlotBackground(canvas, width, height) {
+let AudioContext = window.AudioContext || window.webkitAudioContext;    
+
+
+// let fftSize = 16384;
+// let frameCount = 16384;
+
+let fftSize = 32768;
+let frameCount = 16384;
+
+let audioCtx;
+let scriptNode;
+let arrayNotes = []
+
+
+function drawCanvasBackground(canvas, width, height) {
   let canvasCtx = canvas.getContext("2d");
 
   canvasCtx.fillStyle = 'rgb(200, 200, 200)';
   canvasCtx.fillRect(0, 0, width, height);
 }
 window.addEventListener('DOMContentLoaded', (event) => {
-  drawPlotBackground(canvasTime, 1000, 100);
-  drawPlotBackground(canvasFreq, 1000, 100);
+  drawCanvasBackground(canvasTime, 1000, 100);
+  drawCanvasBackground(canvasFreq, 1000, 100);
 
   $("#turn_on_microphone")[0].onclick = () => {
-    console.log('clicked');
-    microphone_off = ! microphone_off;
-    $("#turn_on_microphone")[0].innerHTML = microphone_off ? 'turn on microphone' : 'turn off microphone';
-    if ( ! microphone_off) {
+    microphoneOff = ! microphoneOff;
+    console.log('microphoneOff =' + microphoneOff);
+    $("#turn_on_microphone")[0].innerHTML = microphoneOff ? 'turn on microphone' : 'turn off microphone';
+    if ( ! microphoneOff) {
       if (gumStream) {
         gumStream.getTracks().forEach(
           (track) => { 
@@ -33,18 +52,26 @@ window.addEventListener('DOMContentLoaded', (event) => {
           function(stream) {
             gumStream = stream;
             console.log('started stream ' + gumStream);
-            // $('#start_analyzing')
-          handleSuccess(gumStream);
+            audioCtx = new AudioContext();
+            scriptNode = audioCtx.createScriptProcessor(frameCount, 1, 1);
+            scriptNode.connect(audioCtx.destination);
+            handleSuccess(gumStream);
 
           }
+        ).catch(
+          (e) => alert('Your browser does not support navigator.mediaDevices.getUserMedia.'
+             + 'Additional error message:' + e
+          )
         );
       }
+      $('#start_analyzing').removeAttr('disabled');;
 
     } else {
       gumStream.getTracks().forEach(
         (track) => { 
           console.log('stopping track' + track);
           track.enabled = false;
+          $('#start_analyzing').attr('disabled','disabled');
         }
       )
     }
@@ -64,15 +91,11 @@ function handleSuccess(stream) {
     console.log('stream not active');
     return ;
   }
-  const audioCtx = new AudioContext();
-  let fftSize = 16384;
-  let frameCount = 16384;
+  audioCtx.resume() 
   let numBuffers = fftSize / frameCount;
   const source = audioCtx.createMediaStreamSource(stream);
 
-  let scriptNode = audioCtx.createScriptProcessor(frameCount, 1, 1);
   source.connect(scriptNode);
-  scriptNode.connect(audioCtx.destination);
   source.onended = function() {
     source.disconnect(scriptNode);
     scriptNode.disconnect(audioCtx.destination);
@@ -84,24 +107,25 @@ function handleSuccess(stream) {
   let bufferCount = 0;
 
   scriptNode.onaudioprocess = function(audioProcessingEvent) {
-
-    let inputBuffer = audioProcessingEvent.inputBuffer;
-    let channelData = inputBuffer.getChannelData(0);
-    // console.log('inputBuffer.getChannelData(0).length=' + channelData.length)
-    input = input.concat(...channelData);
-
-    let inputVisualize = input.map(
-      x => x*1. *25
-    )
-    visualize(canvasTime, inputVisualize, 1000, 100, 50);
-
-    if (stopped) {
+    if (microphoneOff) {
       return ;
     }
-
+    let inputBuffer = audioProcessingEvent.inputBuffer;
+    let channelData = inputBuffer.getChannelData(0);
+    input = input.concat(...channelData);
     bufferCount++;
-    // console.log("input.length=" +input.length)
     if (bufferCount < numBuffers) {
+      return ;
+    }
+    let timeSeries = input;
+    input = [];
+    bufferCount = 0;
+    let timeSeriesVisualize = timeSeries.map(
+      x => x*1. *25
+    )
+    visualize(canvasTime, timeSeriesVisualize, 512, 1000, 100, 50);
+
+    if (stopped) {
       return ;
     }
 
@@ -116,42 +140,40 @@ function handleSuccess(stream) {
 
     //
 
-    let res = fft.analyze(input);
+    let res = fft.analyze(timeSeries);
     res = fft.toMagnitude(res).slice(0, res.length/2);
     let freqLow = 20;
     let freqHigh = 8000;
     let factor = 44100./(fftSize);
     let low = freqLow/factor;
     let high = freqHigh/factor;
+
     let pitch = fft.getArgmax(res)*factor;
     res  = res.slice(low, high)
 
     let measureFreqText = document.querySelector('#measured_freq');
     measureFreqText.innerHTML = (Math.round(pitch*100)/100.).toString();
-
-    let peakVal = fft.getMax(res);
-    // console.log('pitch=' + pitch)
-    // console.log('peakVal=' + peakVal)
-
+    
+    
+    let subsampleRate = 64;
+    let subsampledRes = res.filter( (value, index, arr) => {
+      return index % subsampleRate == 0;
+    });
+    let peakVal = fft.getMax(subsampledRes);
     let resVisualize = res.map(
       x => x*1. / peakVal*100
     )
-
-    visualize(canvasFreq, resVisualize, 1000, 100, 0);
-    input = [];
-    bufferCount = 0;
-
+    visualize(canvasFreq, resVisualize, subsampleRate, 1000, 100, 0);
   }
 }
 
 
 // draw an oscilloscope of the current audio source
-function visualize(canvas, dataArray, width, height, offset) {
+function visualize(canvas, dataArray, subsampleRate, width, height, offset) {
 
   let draw = () => {
     let canvasCtx = canvas.getContext("2d");
     drawVisual = requestAnimationFrame(draw);
-  
   
     canvasCtx.fillStyle = 'rgb(200, 200, 200)';
     canvasCtx.fillRect(0, 0, width, height);
@@ -161,10 +183,9 @@ function visualize(canvas, dataArray, width, height, offset) {
   
     canvasCtx.beginPath();
     let dataLength = dataArray.length;
-    var sliceWidth = width * 1.0 / dataLength;
+    var sliceWidth = width * 1.0 / dataLength * subsampleRate;
     var x = 0;
-  
-    for(var i = 0; i < dataLength; i++) {
+    for(var i = 0; i < dataLength; i += subsampleRate) {
   
       var v = offset + dataArray[i];
       var y = height - v;
@@ -176,7 +197,6 @@ function visualize(canvas, dataArray, width, height, offset) {
       }
       x += sliceWidth;
     }
-  
     canvasCtx.stroke();
   };
 
